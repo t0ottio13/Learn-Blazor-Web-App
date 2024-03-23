a8f7c5b9d564441b6b87c84df8e2f64656367961
Toshimasa Hashiguchi <adsoption104@gmail.com>
2024-03-24 00:59:24 +0900

    全選択ボタン付きチェックボックスを追加

---
+++ Learn-Blazor-WebAssembly/Samples/ThreeStatesListForm/FormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learn_Blazor_WebAssembly.Samples.ThreeStatesListForm;
+
+public class FormModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = string.Empty;
+
+    [MinLength(1, ErrorMessage = "Item not selected")]
+    public List<string> SelectedLists { get; set; } = [];
+}
